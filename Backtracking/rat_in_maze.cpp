2860cc4151a01c5effcf6552336d3276cf889708
think20 <suicidesquade21@gmail.com>
2018-06-13 22:52:20 +0530

    added rat in a maze problem

---
+++ Backtracking/rat_in_maze.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <climits>
+#include <map>
+#include <string>
+
+using namespace std;
+
+typedef long long int lint;
+
+bool isSafe(vector <vector <int> > &MAZE, int x, int y) {
+	int N = MAZE.size();
+	if (x >= 0 && y >= 0 && x < N && y < N && MAZE[x][y] == 1) {
+		return true;
+	}
+	return false;
+}
+
+bool mazeHelper(vector <vector <int> > &MAZE, int x, int y, vector<vector <int> >&Path) {
+	// base case
+	int N = Path.size();
+	if (x == N - 1 && y == N - 1) {		//destination
+		Path[x][y] = 1;
+		return true;
+	}
+	if (isSafe(MAZE, x, y)) {
+		//choose
+		Path[x][y] = 1;
+
+		// explore
+		if (mazeHelper(MAZE, x + 1, y, Path)) { 	// explore in x
+			return true;
+		}
+		if (mazeHelper(MAZE, x, y + 1, Path)) {		// explore in y
+			return true;
+		}
+
+		//unchoose : backtrack
+		Path[x][y] = 0;
+		return false;
+	}
+	return false;
+}
+
+void solveMaze(vector <vector <int> > &MAZE) {
+	int N = MAZE.size();
+	vector <vector <int> >Path(N, vector<int> (N, 0));
+	if (mazeHelper(MAZE, 0, 0, Path)) {
+		// print path
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				cout << Path[i][j] << " ";
+			}
+			cout << endl;
+		}
+	} else {
+		cout << "No Solution\n";
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector <vector <int> >MAZE(N, vector<int> (N, 0));
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> MAZE[i][j];
+		}
+	}
+	solveMaze(MAZE);
+	return 0;
+}
