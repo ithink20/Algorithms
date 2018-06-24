41879f744883d68630cb1c91394f001df2c2b536
Vikas Chaurasiya <vikaschaurasiya@MacBook-Pro.local>
2018-06-25 00:30:29 +0530

    replaced file

--- Graph/kruskal.cpp
+++ Graph/kruskal.cpp
@@ -1,64 +1,72 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
 #include <algorithm>
 #include <vector>
+#include <set>
+#include <map>
 #include <utility>
 
 using namespace std;
-
+typedef long long int lint;
 const int MAX = 1e4 + 5;
-int A[MAX];
 
-void initialize(int *A) {
-	for (int i = 0; i < MAX; ++i) {
-		A[i] = i;
-	}
+// Time Complexity : O(MlogN), M - egdes, N - vertices
+
+void initialise(int *id) {
+    for (int i = 0; i < MAX; ++i) {
+        id[i] = i;
+    }
 }
 
-int node(int x) {
-	while (A[x] != x) {
-		A[x] = A[A[x]];
-		x = A[x];
-	}
-	return x;
+int root(int *id, int i) {
+    while (id[i] != i) {
+        id[i] = id[id[i]];
+        i = id[i];
+    }
+    return i;
 }
 
-void _union(int x, int y) {
-	int s = node(x);
-	int t = node(y);
-	A[s] = A[t];
+void _union(int *id, int x, int y) {
+    int root_x = root(id, x);
+    int root_y = root(id, y);
+    id[root_x] = id[root_y];
 }
 
-int Kruskal(pair <long long, pair<int, int> > p[], int edges) {
-	int x, y;
-	int cost, min_cost = 0;
-	for (int i = 0; i < edges; ++i) {
-		x = p[i].second.first;
-		y = p[i].second.second;
-		cost = p[i].first;
-		
-		// checking cycle...
-		if (node(x) != node(y)) {
-			min_cost += cost;
-			_union(x, y);
-			cout << x << " " << y << " ";
-		}
-	}
-	cout << endl;
-	return min_cost;
+int helper(pair<int , pair<int, int> > *A, int *id, int M) {
+    int x, y, cost, minimum_cost = 0;
+    for (int i = 0; i < M; ++i) {
+
+        //pick each edge in increasing order(sorted).
+        x = A[i].second.first;
+        y = A[i].second.second;
+        cost = A[i].first;
+        //check if selecting edge is creating cycle or not.
+        if (root(id, x) != root(id, y)) {
+            minimum_cost += cost;
+            cout << "{" << x << " " << y << " : " << cost << "} ";
+            _union(id, x, y);
+        }
+    }
+    return minimum_cost;
+}
+
+int kruskal(pair<int , pair<int, int> > *A, int M) {
+    int id[MAX];
+    initialise(id);
+    return helper(A, id, M);
 }
 
 int main() {
-	int u, v, x, y;
-	int weight, cost, min_cost;
-	int nodes, edges;
-	initialize(A);
-	cin >> nodes >> edges;
-	pair <long long, pair<int, int> > p[MAX];
-	for (int i = 0; i &lt; edges; ++i) {
-		cin >> x >> y >> weight;
-		p[i] = make_pair(weight, make_pair(x, y));
-	}
-	sort(p, p + edges);
-	cout << "Minimum Cost : " << Kruskal(p, edges) << endl;
-	return 0;
+    pair<int , pair<int, int> >A[MAX];
+    int N, M;
+    cin >> N >> M;
+    int u, v, wt;
+    for (int i = 0; i < M; ++i) {
+        cin >> u >> v >> wt;
+        A[i] = make_pair(wt, make_pair(u, v));
+    }
+    sort(A, A + M);
+    cout << "Minimum cost : " << kruskal(A, M) << endl;
+    return 0;
 }
