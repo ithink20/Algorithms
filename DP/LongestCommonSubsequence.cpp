d90024a148e4f4bc01a8ec2a894033b9d031f975
ithink20 <suicidesquade21@gmail.com>
2018-08-27 14:52:03 +0530

    updating print string

--- DP/LongestCommonSubsequence.cpp
+++ DP/LongestCommonSubsequence.cpp
@@ -26,11 +26,34 @@ int lcs_helper(vector<vector<int> >&dp, string A, string B, int m, int n) {
     }
 }
 
+void printLcs(vector<vector<int> >&dp, string A, string B, int m, int n) {
+    int last_index = dp[m][n];
+    char matched_string[last_index + 1];  //to store matched string
+    matched_string[last_index] = '\0';
+
+    for(int i = m, j = n; i > 0 && j > 0; ) {
+        if (A[i - 1] == B[j - 1]) {
+            matched_string[last_index - 1] = A[i - 1];
+            last_index -= 1;
+            i -= 1;
+            j -= 1;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+    cout << matched_string << endl;
+}
+
 int lcs(string A, string B) {
     int m = A.length();
     int n = B.length();
     vector <vector<int> >dp(m + 1, vector<int>(n + 1, -1));
-    return lcs_helper(dp, A, B, m, n);
+    int ans = lcs_helper(dp, A, B, m, n);
+    printLcs(dp, A, B, m, n);
+    return ans;
 }
 
 
